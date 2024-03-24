82e6875d3049555dbe33b917e1121b0e82c2838c
karkiyubin <therealyubin@gmail.com>
2024-03-24 16:52:12 +0545

    add hostname

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 import config from "../config.js";
 
-app.listen(3000, () =>{
-    console.log("Started on Port 3000")
-})
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Started on Port 3000");
+});
